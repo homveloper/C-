c4e20e592b2d60262ea125689770f2d8ca098192
totomo <bum4496@naver.com>
2020-07-15 21:07:44 +0900

    02 연습문제

--- test.cpp
+++ test.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 int main(){
-    int x,y = 0;            //x와 y 모두 0으로 초기화
-    char grade = 'A';       //문자 A의 아스키 코드를 grade에 대입
-    double rate = 10^10;    //rate에 1*10^10을 대입
-    int salary = 2,000,000; //salary에 2,000,000을 대입
+    int x = 10, y = 3,  z = 2;
 
-    cout.setf(ios_base::fixed);
-    cout<<rate<<endl;
+    x += y -= 2;
 
-    return 0;
+    cout<<"x = "<<x<<"  y = "<<y<<"   z = "<<z<<endl;
+    x = 10, y = 3, z = 2;
+
+    x *= y + 2;
+
+    cout<<"x = "<<x<<"  y = "<<y<<"   z = "<<z<<endl;
+    x = 10, y = 3, z = 2;
+
+    z = ++x + ++y;
+
+    cout<<"x = "<<x<<"  y = "<<y<<"   z = "<<z<<endl;
+    x = 10, y = 3, z = 2;
+
+    z = x++ + ++y;
+
+    cout<<"x = "<<x<<"  y = "<<y<<"   z = "<<z<<endl;
 }
