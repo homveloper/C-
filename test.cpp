e46fd482f36b28f26e499d46e00738ffb185fca6
totomo <bum4496@naver.com>
2020-07-15 20:42:27 +0900

    d

--- test.cpp
+++ test.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include "iostream"
 using namespace std;
 
-int main()
-{
-	int a = 2;
-	a = ++a * 20;
-	cout << a << endl;
+int main(){
+    int x,y = 0;            //x와 y 모두 0으로 초기화
+    char grade = 'A';       //문자 A의 아스키 코드를 grade에 대입
+    double rate = 10^10;    //rate에 1*10^10을 대입
+    int salary = 2,000,000; //salary에 2,000,000을 대입
 
-	a = 2;
+    cout.setf(ios_base::fixed);
+    cout<<rate<<endl;
 
-	a = a++ * 20;
-	cout << a << endl;
+    return 0;
 }
