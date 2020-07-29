33bb9e4e7f8efd1cf8ecfcaec474bfc55538a7b5
totomo <bum4496@nave.com>
2020-07-29 16:28:38 +0900

    ㅇ

--- test.cpp
+++ test.cpp
@@ -1,27 +1,12 @@
 #include <iostream>
+#include <typeinfo>
+#include <cstring>
 using namespace std;
 
-void swap(int x, int y){	//인자를 값으로 받음
-	
-    int temp = x;
-	x = y;
-	y = temp;
-}
+void main()
+{
+    string word = "1234";
+    
 
-
-
-
-
-void swap2(int *x, int *y){	//인자를 포인터로 받음
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-void main(){
-	int a = 100, b = 200;
-
-	cout<<a<<" "<<b<<endl;
-	swap2(&a,&b);
-	cout<<a<<" "<<b<<endl;
+    cout<<word.size()<<endl;
 }
